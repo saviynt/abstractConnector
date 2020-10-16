811799d14b7798fab1c0eaf1c0826ff41c0fa5da
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:09:58 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/exceptions/ConnectorException.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/exceptions/ConnectorException.java
@@ -1,30 +1,64 @@
 
 package com.saviynt.ssm.abstractConnector.exceptions;
 
+/**
+ * The Class ConnectorException.
+ */
 public class ConnectorException extends RuntimeException {
 
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Instantiates a new connector exception.
+	 */
 	public ConnectorException() {
 		super();
 	}
 
+	/**
+	 * Instantiates a new connector exception.
+	 *
+	 * @param message the message
+	 */
 	public ConnectorException(String message) {
 		super(message);
 	}
 
+	/**
+	 * Instantiates a new connector exception.
+	 *
+	 * @param originalException the original exception
+	 */
 	public ConnectorException(Throwable originalException) {
 		super(originalException);
 	}
 
+	/**
+	 * Instantiates a new connector exception.
+	 *
+	 * @param message the message
+	 * @param originalException the original exception
+	 */
 	public ConnectorException(String message, Throwable originalException) {
 		super(message, originalException);
 	}
 
+	/**
+	 * Rethrow.
+	 *
+	 * @throws Throwable the throwable
+	 */
 	public void rethrow() throws Throwable {
 		throw (getCause() == null) ? this : getCause();
 	}
 
+	/**
+	 * Wrap.
+	 *
+	 * @param ex the ex
+	 * @return the runtime exception
+	 */
 	public static RuntimeException wrap(Throwable ex) {
 
 		if (ex instanceof Error) {
