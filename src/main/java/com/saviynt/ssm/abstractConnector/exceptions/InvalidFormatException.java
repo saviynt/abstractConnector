0d39caab09c130fad8f83921711d3847ccf0228f
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:15:37 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/exceptions/InvalidFormatException.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/exceptions/InvalidFormatException.java
@@ -1,25 +1,49 @@
 package com.saviynt.ssm.abstractConnector.exceptions;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class InvalidFormatException.
+ */
 public class InvalidFormatException extends ConnectorException{
 	
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
     
     
+    /**
+     * Instantiates a new invalid format exception.
+     */
     public InvalidFormatException() {
         super();
     }
 
     
+    /**
+     * Instantiates a new invalid format exception.
+     *
+     * @param message the message
+     */
     public InvalidFormatException(String message) {
         super(message);
     }
 
      
+    /**
+     * Instantiates a new invalid format exception.
+     *
+     * @param cause the cause
+     */
     public InvalidFormatException(Throwable cause) {
         super(cause);
     }
 
    
+    /**
+     * Instantiates a new invalid format exception.
+     *
+     * @param message the message
+     * @param cause the cause
+     */
     public InvalidFormatException(String message, Throwable cause) {
         super(message, cause);
     }
