c0deb0c1d61226788fe75785933d841b6ecdf8bf
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:11:34 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/exceptions/InvalidAttributeValueException.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/exceptions/InvalidAttributeValueException.java
@@ -2,26 +2,49 @@
 
 package com.saviynt.ssm.abstractConnector.exceptions;
 
+/**
+ * The Class InvalidAttributeValueException.
+ */
 public class InvalidAttributeValueException extends ConnectorException {
 
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
     
        
+    /**
+     * Instantiates a new invalid attribute value exception.
+     */
     public InvalidAttributeValueException() {
         super();
     }
 
     
+    /**
+     * Instantiates a new invalid attribute value exception.
+     *
+     * @param message the message
+     */
     public InvalidAttributeValueException(String message) {
         super(message);
     }
 
      
+    /**
+     * Instantiates a new invalid attribute value exception.
+     *
+     * @param cause the cause
+     */
     public InvalidAttributeValueException(Throwable cause) {
         super(cause);
     }
 
    
+    /**
+     * Instantiates a new invalid attribute value exception.
+     *
+     * @param message the message
+     * @param cause the cause
+     */
     public InvalidAttributeValueException(String message, Throwable cause) {
         super(message, cause);
     }
