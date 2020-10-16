4da247c3a56da7316c633cebf20c3d8d47a0e71a
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:19:29 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/exceptions/OperationTimeoutException.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/exceptions/OperationTimeoutException.java
@@ -1,22 +1,45 @@
 
 package com.saviynt.ssm.abstractConnector.exceptions;
 
+/**
+ * The Class OperationTimeoutException.
+ */
 public class OperationTimeoutException extends ConnectorException {
 
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Instantiates a new operation timeout exception.
+	 */
 	public OperationTimeoutException() {
 		super();
 	}
 
+	/**
+	 * Instantiates a new operation timeout exception.
+	 *
+	 * @param msg the msg
+	 */
 	public OperationTimeoutException(String msg) {
 		super(msg);
 	}
 
+	/**
+	 * Instantiates a new operation timeout exception.
+	 *
+	 * @param e the e
+	 */
 	public OperationTimeoutException(Throwable e) {
 		super(e);
 	}
 
+	/**
+	 * Instantiates a new operation timeout exception.
+	 *
+	 * @param msg the msg
+	 * @param e the e
+	 */
 	public OperationTimeoutException(String msg, Throwable e) {
 		super(msg, e);
 	}
