e9a9efef3d63ecaf9791f746f56add0401efceef
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:04:01 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/ExposedObject.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/ExposedObject.java
@@ -1,14 +1,30 @@
 package com.saviynt.ssm.abstractConnector;
 
+/**
+ * The Enum ExposedObject.
+ */
 public enum ExposedObject {
+	
+	/** The users. */
 	USERS("Users");
 
+	/** The name. */
 	private String name;
 
+	/**
+	 * Instantiates a new exposed object.
+	 *
+	 * @param name the name
+	 */
 	ExposedObject(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
