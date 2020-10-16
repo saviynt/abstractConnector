a2bf1241321d6ce7c86506856fbe7c421af9ee81
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:09:06 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/utility/GroovyService.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/utility/GroovyService.java
@@ -6,9 +6,22 @@ import java.util.Map;
 import groovy.lang.Writable;
 import groovy.text.SimpleTemplateEngine;
 
+/**
+ * The Class GroovyService.
+ */
 public class GroovyService {
+	
+	/** The Constant SIMPLE_TEMPLATE_ENGINE. */
 	public static final SimpleTemplateEngine SIMPLE_TEMPLATE_ENGINE = new SimpleTemplateEngine();
 
+	/**
+	 * Convert template to string.
+	 *
+	 * @param text the text
+	 * @param model the model
+	 * @return the string
+	 * @throws Exception the exception
+	 */
 	public static String convertTemplateToString(String text, Map<String, Object> model) throws Exception {
 		if (text == null) {
 			return null;
