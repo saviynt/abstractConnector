90504732103a7f7ea0acaf8581b4fa82fd3d7379
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:17:56 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/exceptions/MissingKeyException.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/exceptions/MissingKeyException.java
@@ -2,26 +2,49 @@
 
 package com.saviynt.ssm.abstractConnector.exceptions;
 
+/**
+ * The Class MissingKeyException.
+ */
 public class MissingKeyException extends ConnectorException {
 
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
     
       
+    /**
+     * Instantiates a new missing key exception.
+     */
     public MissingKeyException() {
         super();
     }
 
     
+    /**
+     * Instantiates a new missing key exception.
+     *
+     * @param message the message
+     */
     public MissingKeyException(String message) {
         super(message);
     }
 
      
+    /**
+     * Instantiates a new missing key exception.
+     *
+     * @param cause the cause
+     */
     public MissingKeyException(Throwable cause) {
         super(cause);
     }
 
    
+    /**
+     * Instantiates a new missing key exception.
+     *
+     * @param message the message
+     * @param cause the cause
+     */
     public MissingKeyException(String message, Throwable cause) {
         super(message, cause);
     }
