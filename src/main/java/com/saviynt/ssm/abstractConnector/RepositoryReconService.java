cc6240535c7b8530fce36f31c1d8ece3890b0ccd
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:05:32 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/RepositoryReconService.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/RepositoryReconService.java
@@ -8,19 +8,47 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+/**
+ * The Class RepositoryReconService.
+ */
 public abstract class RepositoryReconService implements ApplicationContextAware {
 
+	/** The application context. */
 	public static ApplicationContext applicationContext = null;
 
+	/**
+	 * Sets the application context.
+	 *
+	 * @param applicationContextObj the new application context
+	 * @throws BeansException the beans exception
+	 */
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContextObj) throws BeansException {
 		applicationContext = applicationContextObj;
 
 	}
 
+	/**
+	 * Update repositary.
+	 *
+	 * @param data the data
+	 * @param endpoints the endpoints
+	 * @param formatterClass the formatter class
+	 * @param dataFromSSM the data from SSM
+	 * @throws Exception the exception
+	 */
 	public abstract void updateRepositary(List<List<Map<String, Object>>> data, Long endpoints, String formatterClass,Map<String,Object> dataFromSSM)
 			throws Exception;
 
+	/**
+	 * Notify.
+	 *
+	 * @param data the data
+	 * @param endpointId the endpoint id
+	 * @param formatterClass the formatter class
+	 * @param dataFromSSM the data from SSM
+	 * @throws Exception the exception
+	 */
 	public static void notify(List<List<Map<String, Object>>> data, Long endpointId, String formatterClass,Map<String,Object> dataFromSSM)
 			throws Exception {
 		RepositoryReconService reconService = applicationContext.getBean(RepositoryReconService.class);
@@ -29,8 +57,22 @@ public abstract class RepositoryReconService implements ApplicationContextAware 
 	}
 
 	 
+	/**
+	 * Gets the last run date from accounts.
+	 *
+	 * @param incrField the incr field
+	 * @return the last run date from accounts
+	 * @throws Exception the exception
+	 */
 	public abstract Date getLastRunDateFromAccounts(String incrField) throws Exception;
 
+	/**
+	 * Gets the last run date.
+	 *
+	 * @param incrField the incr field
+	 * @return the last run date
+	 * @throws Exception the exception
+	 */
 	public static Date getLastRunDate(String incrField) throws Exception {
 		RepositoryReconService reconService = applicationContext.getBean(RepositoryReconService.class);
 		return reconService.getLastRunDateFromAccounts(incrField);
