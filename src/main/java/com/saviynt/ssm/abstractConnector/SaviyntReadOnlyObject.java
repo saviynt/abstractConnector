7f1d9fc595f3cae059fb5196b6e1b4bc1a6b1063
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:06:07 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/SaviyntReadOnlyObject.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/SaviyntReadOnlyObject.java
@@ -3,8 +3,20 @@ package com.saviynt.ssm.abstractConnector;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The Class SaviyntReadOnlyObject.
+ */
 public abstract class SaviyntReadOnlyObject {
 
+	/**
+	 * Gets the object list.
+	 *
+	 * @param sObject the s object
+	 * @param filter the filter
+	 * @param firstResult the first result
+	 * @param maxResult the max result
+	 * @return the object list
+	 */
 	@SuppressWarnings("rawtypes")
 	public abstract List getObjectList(ExposedObject sObject,Map<String, Object> filter,Integer firstResult,Integer maxResult);
 	
