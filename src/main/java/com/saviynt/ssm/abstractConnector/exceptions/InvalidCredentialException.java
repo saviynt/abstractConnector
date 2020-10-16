b5981ba959f5a723824525a438576467d941546a
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:14:09 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/exceptions/InvalidCredentialException.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/exceptions/InvalidCredentialException.java
@@ -2,25 +2,48 @@
  
 package com.saviynt.ssm.abstractConnector.exceptions;
  
+/**
+ * The Class InvalidCredentialException.
+ */
 public class InvalidCredentialException extends ConnectorException {
 
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
 
    
+    /**
+     * Instantiates a new invalid credential exception.
+     */
     public InvalidCredentialException() {
         super();
     }
 
     
+    /**
+     * Instantiates a new invalid credential exception.
+     *
+     * @param message the message
+     */
     public InvalidCredentialException(String message) {
         super(message);
     }
  
+    /**
+     * Instantiates a new invalid credential exception.
+     *
+     * @param ex the ex
+     */
     public InvalidCredentialException(Throwable ex) {
         super(ex);
     }
 
     
+    /**
+     * Instantiates a new invalid credential exception.
+     *
+     * @param message the message
+     * @param ex the ex
+     */
     public InvalidCredentialException(String message, Throwable ex) {
         super(message, ex);
     }
