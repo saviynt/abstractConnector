2648767abae16d980c721ef4b7a6f455682e4b1b
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:17:07 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/exceptions/InvalidIdentityTypeException.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/exceptions/InvalidIdentityTypeException.java
@@ -2,29 +2,52 @@
 
 package com.saviynt.ssm.abstractConnector.exceptions;
 
+/**
+ * The Class InvalidIdentityTypeException.
+ */
 public class InvalidIdentityTypeException extends ConnectorException {
 
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
     
     
     
 
     
+    /**
+     * Instantiates a new invalid identity type exception.
+     */
     public InvalidIdentityTypeException() {
         super();
     }
 
     
+    /**
+     * Instantiates a new invalid identity type exception.
+     *
+     * @param message the message
+     */
     public InvalidIdentityTypeException(String message) {
         super(message);
     }
 
      
+    /**
+     * Instantiates a new invalid identity type exception.
+     *
+     * @param cause the cause
+     */
     public InvalidIdentityTypeException(Throwable cause) {
         super(cause);
     }
 
    
+    /**
+     * Instantiates a new invalid identity type exception.
+     *
+     * @param message the message
+     * @param cause the cause
+     */
     public InvalidIdentityTypeException(String message, Throwable cause) {
         super(message, cause);
     }
