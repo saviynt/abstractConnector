6fe20087c79ce7b8b274a754216146482cc0d64c
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:10:42 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/exceptions/IdentityTypeNotSupportedException.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/exceptions/IdentityTypeNotSupportedException.java
@@ -2,28 +2,51 @@
 
 package com.saviynt.ssm.abstractConnector.exceptions;
 
+/**
+ * The Class IdentityTypeNotSupportedException.
+ */
 public class IdentityTypeNotSupportedException extends ConnectorException {
 
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
     
     
    
     
+    /**
+     * Instantiates a new identity type not supported exception.
+     */
     public IdentityTypeNotSupportedException() {
         super();
     }
 
     
+    /**
+     * Instantiates a new identity type not supported exception.
+     *
+     * @param message the message
+     */
     public IdentityTypeNotSupportedException(String message) {
         super(message);
     }
 
      
+    /**
+     * Instantiates a new identity type not supported exception.
+     *
+     * @param cause the cause
+     */
     public IdentityTypeNotSupportedException(Throwable cause) {
         super(cause);
     }
 
    
+    /**
+     * Instantiates a new identity type not supported exception.
+     *
+     * @param message the message
+     * @param cause the cause
+     */
     public IdentityTypeNotSupportedException(String message, Throwable cause) {
         super(message, cause);
     }
