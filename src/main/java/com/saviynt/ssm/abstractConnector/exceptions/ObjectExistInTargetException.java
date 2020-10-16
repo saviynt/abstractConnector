eecf8c7a2d6973f02fcaf33ee07e4f97a048d549
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-15 23:18:43 -0700

    javadoc added

--- src/main/java/com/saviynt/ssm/abstractConnector/exceptions/ObjectExistInTargetException.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/exceptions/ObjectExistInTargetException.java
@@ -1,21 +1,44 @@
 package com.saviynt.ssm.abstractConnector.exceptions;
 
+/**
+ * The Class ObjectExistInTargetException.
+ */
 public class ObjectExistInTargetException extends ConnectorException {
 
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Instantiates a new object exist in target exception.
+	 */
 	public ObjectExistInTargetException() {
 		super();
 	}
 
+	/**
+	 * Instantiates a new object exist in target exception.
+	 *
+	 * @param message the message
+	 */
 	public ObjectExistInTargetException(String message) {
 		super(message);
 	}
 
+	/**
+	 * Instantiates a new object exist in target exception.
+	 *
+	 * @param ex the ex
+	 */
 	public ObjectExistInTargetException(Throwable ex) {
 		super(ex);
 	}
 
+	/**
+	 * Instantiates a new object exist in target exception.
+	 *
+	 * @param message the message
+	 * @param ex the ex
+	 */
 	public ObjectExistInTargetException(String message, Throwable ex) {
 		super(message, ex);
 	}
