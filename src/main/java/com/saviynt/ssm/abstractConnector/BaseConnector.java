ad672cc461e43eea28a8be2b36a9d575a5678815
saviynt <70003347+saviynt@users.noreply.github.com>
2020-10-20 12:47:28 -0700

    description added

--- src/main/java/com/saviynt/ssm/abstractConnector/BaseConnector.java
+++ src/main/java/com/saviynt/ssm/abstractConnector/BaseConnector.java
@@ -1,7 +1,5 @@
-
 package com.saviynt.ssm.abstractConnector;
 
-import java.util.List;
 import java.util.Map;
 
 import com.saviynt.ssm.abstractConnector.exceptions.ConnectorException;
@@ -11,13 +9,13 @@ import com.saviynt.ssm.abstractConnector.exceptions.MissingKeyException;
 import com.saviynt.ssm.abstractConnector.exceptions.OperationTimeoutException;
 
  
- /**
+/**
  * The Interface BaseConnector.
  */
 interface BaseConnector {
 	
 	/**
-	 * Gets the logon id and session id by client name.
+	 * purpose: to get the logon id and session id by client name
 	 *
 	 * @param configData the config data
 	 * @param data the data
@@ -27,7 +25,7 @@ interface BaseConnector {
 	public String getLogonIdAndSessionIdByClientName(Map<String, Object> configData, Map<String, Object> data)  throws ConnectorException;
    
 	/**
-	 * Gets the logoff by session id.
+	 * purpose: to get the logoff by session id
 	 *
 	 * @param configData the config data
 	 * @param data the data
@@ -37,7 +35,7 @@ interface BaseConnector {
 	public Boolean getLogoffBySessionId(Map<String, Object> configData, Map<String, Object> data)   throws ConnectorException;
     
 	/**
-	 * Gets the log off disc sessions.
+	 * purpose: to get the log off disc sessions
 	 *
 	 * @param configData the config data
 	 * @return the log off disc sessions
@@ -46,14 +44,14 @@ interface BaseConnector {
 	public Boolean getLogOffDiscSessions(Map<String, Object> configData) throws ConnectorException;
 	
 	/**
-	 * Display name.
+	 *  purpose: to retrieve connector display name
 	 *
 	 * @return the string
 	 */
 	public abstract String displayName();
 
 	/**
-	 * Version.
+	 * purpose: to retrieve connector version
 	 *
 	 * @return the string
 	 */
@@ -62,9 +60,11 @@ interface BaseConnector {
 	/**
 	 * Test.
 	 *
-	 * @param configData the config data
-	 * @param filterData the filter data
-	 * @return the boolean
+	 * purpose: to process reconsile for users and accounts
+	 *
+	 * @param configData the config data  for target connection information
+	 * @param filterData the data from ecm  for  input data from connection 
+	 * @return the boolean true or false
 	 * @throws ConnectorException the connector exception
 	 * @throws InvalidCredentialException the invalid credential exception
 	 * @throws InvalidAttributeValueException the invalid attribute value exception
@@ -74,212 +74,213 @@ interface BaseConnector {
 	public abstract Boolean test(Map<String,Object> configData ,Map<String,Object> filterData) throws ConnectorException,InvalidCredentialException, InvalidAttributeValueException, OperationTimeoutException,MissingKeyException;
 
 	/**
-	 * Reconsile.
+	 * purpose: to process reconsile for users and accounts
 	 *
-	 * @param configData the config data
-	 * @param filterData the filter data
+	 * @param configData the config data  for target connection information
+	 * @param filterData the data from ecm  for  input data from connection 
 	 * @param formatterClass the formatter class
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract void reconsile(Map<String,Object> configData,Map<String, Object> filterData,String formatterClass) throws ConnectorException;
  	 
 	/**
-	 * Check existing.
+	* purpose: to check existing record for the input object.
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @param serachableObject the serachable object
-	 * @return the boolean
+	 * @param configData the config data   for target connection information
+	 * @param data the data   for  input data from connection
+	 * @return the boolean true or flase
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Boolean checkExisting(Map<String,Object> configData,Map<String,Object> data,SearchableObject serachableObject) throws ConnectorException;
 	
 	/**
-	 * Creates the account.
+	 * purpose: to create the account in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @return the boolean
+	 * @param configData the config data   for target connection information
+	 * @param data the data  for  input data from connection
+	 * @return the boolean true or false
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Boolean createAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
-	/**
-	  * Update account.
-	  *
-	  * @param configData the config data
-	  * @param data the data
-	  * @return the integer
-	  * @throws ConnectorException the connector exception
-	  */
-	 public abstract Integer updateAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
 	/**
-	 * Lock account.
+	 * purpose: to update the account in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @return the boolean
+	 * @param configData the config data   for target connection information
+	 * @param data the data  for  input data from connection
+	 * @return the integer number of accounts updated
+	 * @throws ConnectorException the connector exception
+	 */
+	public abstract Integer updateAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
+	
+	/**
+	 * purpose: to lock the account in target system
+	 *
+	 * @param configData the config data   for target connection information
+	 * @param data the data  for  input data from connection
+	 * @return the boolean true or false
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Boolean lockAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
 	/**
-	 * Disable account.
+	 * purpose: to disable the account in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @return the boolean
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the boolean true or false
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Boolean disableAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
-    /**
-     * Un lock account.
-     *
-     * @param configData the config data
-     * @param data the data
-     * @return the boolean
-     * @throws ConnectorException the connector exception
-     */
+	/**
+	 * purpose: to unlock the account in target system
+	 *
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the boolean true or false
+	 * @throws ConnectorException the connector exception
+	 */
     public abstract Boolean unLockAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
-	/**
-	 * Enable account.
+    /**
+	 * purpose: to enable the account in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @return the boolean
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the boolean true or false
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Boolean enableAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
 	/**
-	 * Terminate account.
+	 * purpose: to terminate the account in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @return the integer
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the integer number of accounts terminated
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Integer terminateAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
 	/**
-	 * Removes the account.
+	 * purpose: to remove the account in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @return the integer
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the integer number of accounts removed
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Integer removeAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
  
+
 	/**
-	 * Adds the access to account.
+	 * purpose: to add access to the account in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @return the integer
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the integer access granted count
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Integer addAccessToAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
 	/**
-	 * Removes the access to account.
+	 * purpose: to remove the account access in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
 	 * @return the integer
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Integer removeAccessToAccount(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
- 	/**
-	  * Change password.
-	  *
-	  * @param configData the config data
-	  * @param data the data
-	  * @return the boolean
-	  * @throws ConnectorException the connector exception
-	  */
+	/**
+	 * purpose: to change the password in target system
+	 *
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the boolean true or false
+	 * @throws ConnectorException the connector exception
+	 */
 	 public abstract Boolean changePassword(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
     /**
-     * Creates the user.
-     *
-     * @param configData the config data
-     * @param data the data
-     * @return the boolean
-     * @throws ConnectorException the connector exception
+     * purpose: to create the user in target system
+	 *
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the boolean true or false
+	 * @throws ConnectorException the connector exception
      */
     public abstract Boolean createUser(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
-	/**
-	 * Update user.
+    /**
+	 * purpose: to update the user in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
 	 * @return the integer
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Integer updateUser(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
 	/**
-	 * Update entitlement.
+	 * purpose: to update the entitlement in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
 	 * @return the integer
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Integer updateEntitlement(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
 	/**
-	 * Creates the entitlement.
+	 * purpose: to create the entitlement in target system
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @return the boolean
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the boolean true or false
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Boolean createEntitlement(Map<String,Object> configData,Map<String,Object> data) throws ConnectorException;
 	
 	/**
-	 * Validate credentials.
+	 * purpose: to validate credentials of the given input from connection
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @return the boolean
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the boolean true or false
 	 * @throws ConnectorException the connector exception
 	 */
 	public abstract Boolean validateCredentials(Map<String,Object> configData, Map<String,Object> data) throws ConnectorException;
 	
 	/**
-	 * Gets the summary.
+	 * purpose: to get the summary of number of records for the given input object such as accounts
 	 *
-	 * @param configData the config data
-	 * @param data the data
-	 * @return the summary
+	 * @param configData the config data for target connection information
+	 * @param data the data for input data from connection 
+	 * @return the summary map with object and count as key ,value 
 	 */
 	public abstract  Map<String, Object> getSummary(Map<String,Object> configData, Map<String,Object> data);
 	
 	/**
-	 * Sets the config.
+	 * purpose: to set the config with attributes needed for creating a connection
 	 *
-	 * @param configData the new config
+	 * @param configData the new config for input connection attributes from connection created
 	 */
 	public abstract  void setConfig(ConfigDataVo configData);
 	
 	/**
-	 * Gets the config.
+	 * purpose: to get the config of connection attributes for creating a connection
 	 *
-	 * @return the config
+	 * @return the new config for input connection attributes from connection created
 	 */
 	public abstract  ConfigDataVo getConfig();
     	 
  	/**
-	  * Checks if is premises.
+	  * purpose: to checks if is premises
 	  *
-	  * @return the boolean
+	  * @return the boolean true or flase
 	  */
 	 public Boolean isPremises();
 	
